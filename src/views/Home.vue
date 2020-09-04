bfad296afacd14e85b2901431dd22e4c1d869488
Christopher Leggett <chris@leggett.dev>
2020-09-03 22:20:20 -0400

    Improves carousel with background image.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,14 +9,14 @@
                 indicators
                 background="#ababab"
                 img-width="1024"
-                img-height="480"
+                img-height="580"
                 style="text-shadow: 0.1rem 0.1rem 0.3rem #333;"
                 @sliding-start="onSlideStart"
                 @sliding-end="onSlideEnd"
             >
-            <b-carousel-slide img-blank>
+            <b-carousel-slide img-src="../assets/background.jpg">
             <div class="mx-auto">
-                <div class="d-flex my-5 py-3 align-items-center justify-content-center">
+                <div class="d-flex my-5 align-items-center justify-content-center">
                     <b-img id="logo" class="mr-3" src="../assets/logo.svg" alt="Logo" style="filter: drop-shadow( 0.1rem 0.4rem 0.3rem #333 );"/>
                     <span class="display-3">Devmarks</span>
                 </div>
