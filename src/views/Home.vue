ab95ec2e8c4fa65ca0559544df502c90bfa6d266
Christopher Leggett <chris@leggett.dev>
2020-09-22 22:32:17 -0400

    Adds some placeholder content to Home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <v-row align="center">
+      <v-col cols="6" sm="4">
+      <v-img
+        src="../assets/logo.svg"
+        max-width="250"
+      />
+      </v-col>
+      <v-col cols="6" sm="4">
+      <p class="text-h1">
+        Devmarks
+      </p>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
