f7731f8bee9e14900a9160c245eedec15838643e
Christopher Leggett <chris@leggett.dev>
2021-02-25 08:34:23 -0500

    Some formatting cleanup.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,52 +1,39 @@
 <template>
-  <v-row 
-      align="center"
-      justify="center"
-      class="text-center"
-    >
-      <v-col
-        cols="12"
-        sm="8"
-        md="4"
-      >
-        <v-row justify="center">
-          <v-img
-            src="../assets/logo.svg"
-            max-width="100"
-          />
-        </v-row>
-        <div class="text-h4 text-center">Sign Up</div>
+  <v-row align="center" justify="center" class="text-center">
+    <v-col cols="12" sm="8" md="4">
+      <v-row justify="center">
+        <v-img src="../assets/logo.svg" max-width="100" />
+      </v-row>
+      <div class="text-h4 text-center">Sign Up</div>
 
-        <ul class="error-messages" v-if="registerError">
-          <li>{{ registerError }}</li>
-        </ul>
+      <ul class="error-messages" v-if="registerError">
+        <li>{{ registerError }}</li>
+      </ul>
 
-        <v-form ref="form" @submit="onSubmit">
-            <v-text-field
-              id="input-email"
-              type="email"
-              required
-              v-model="email"
-              placeholder="Email"
-              label="Email address:"
-            ></v-text-field>
-            <v-text-field
-              required
-              id="input-password"
-              type="password"
-              v-model="password"
-              placeholder="Password"
-              label="Password:"
-            ></v-text-field>
-          <v-btn type="submit" color="primary">
-            Sign in
-          </v-btn>
-          <p class="text-right">
-            <router-link to="/login"> Already have an account? </router-link>
-          </p>
-        </v-form>
-      </v-col>
-    </v-row>
+      <v-form ref="form" @submit="onSubmit">
+        <v-text-field
+          id="input-email"
+          type="email"
+          required
+          v-model="email"
+          placeholder="Email"
+          label="Email address:"
+        ></v-text-field>
+        <v-text-field
+          required
+          id="input-password"
+          type="password"
+          v-model="password"
+          placeholder="Password"
+          label="Password:"
+        ></v-text-field>
+        <v-btn type="submit" color="primary"> Sign in </v-btn>
+        <p class="text-right">
+          <router-link to="/login"> Already have an account? </router-link>
+        </p>
+      </v-form>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -66,21 +53,13 @@ export default class Register extends Vue {
     auth
       .register({
         email: this.email,
-        password: this.password
+        password: this.password,
       })
       .then(() => this.$router.push("/login"))
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
         this.registerError = "Invalid username or password";
       });
-  }
-
-  onSlideStart(slide: number) {
-    this.sliding = true;
-  }
-
-  onSlideEnd(slide: number) {
-    this.sliding = false;
   }
 }
 </script>
