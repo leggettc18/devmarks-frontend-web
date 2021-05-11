d1efec25914d0c98a3b6ccb8ac4f02b997994c61
Christopher Leggett <chris@leggett.dev>
2021-05-11 18:33:41 -0400

    Adds a slide-out animation to the sidebar. 🎚️

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,10 @@
 <template>
-  <aside v-if="mountSidebar" class="min-h-screen" :class="`bg-${bgColor}`">
-    <slot></slot>
+  <aside v-if="mountSidebar" class="min-h-screen">
+    <transition name="slide-right" appear @after-leave="close">
+      <div v-if="closeSidebar" class="min-h-screen" :class="`bg-${bgColor}`">
+        <slot></slot>
+      </div>
+    </transition>
   </aside>
 </template>
 
@@ -26,6 +30,7 @@ export default defineComponent({
   emits: ["update:modelValue"],
   setup(props, { emit }) {
     const mountSidebar = ref(props.modelValue);
+    const closeSidebar = ref(props.modelValue);
 
     const close = () => {
       emit("update:modelValue", false);
@@ -35,15 +40,32 @@ export default defineComponent({
     watch(
       () => props.modelValue,
       (value) => {
-        mountSidebar.value = value;
+        if (value) mountSidebar.value = true;
+        closeSidebar.value = value;
       }
     );
 
     return {
       close,
       mountSidebar,
+      closeSidebar,
     };
   },
 });
 </script>
+
+<style>
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  transition-property: transform, left, margin-left;
+  transform: translateX(0%);
+}
+.slide-right-enter,
+.slide-right-leave-to {
+  transform: translateX(-100%);
+  left: 0 !important;
+  margin-left: 0 !important;
+}
+</style>
 
