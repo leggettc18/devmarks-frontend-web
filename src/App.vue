f177c6c0936dfbff5b1de822928ba055cc8bdef0
Christopher Leggett <chris@leggett.dev>
2021-05-15 23:58:36 -0400

    Properly fixes page height issues. 📏

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-screen flex flex-col">
     <navbar bg-color="primary" text-color="white" class="h-auto">
       <template #nav-left>
         <div class="flex space-x-2 pl-2 items-center">
@@ -45,67 +45,65 @@
         </template>
       </template>
     </navbar>
-    <div class="h-full">
-      <div class="flex flex-row bg-gray-100 text-gray-800 h-full">
-        <div class="flex fixed md:relative min-h-full md:min-h-screen">
-          <sidebar v-if="isLoggedIn()" v-model="showSidebar" bg-color="primary" text-color="white">
-            <div router="true" class="sidebar-content px-4 py-4">
-              <ul class="flext flex-col w-full">
-                <li class="my-px">
-                  <router-link
-                    to="/home"
-                    class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-primary-100 hover:text-primary"
-                  >
-                    <span class="flex items-center justify-center text-lg">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                        />
-                      </svg>
-                    </span>
-                    <span class="ml-3">Home</span>
-                  </router-link>
-                </li>
-                <li class="my-px">
-                  <router-link
-                    to="/bookmarks"
-                    class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-primary-100 hover:text-primary"
-                  >
-                    <span class="flex items-center justify-center text-lg">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
-                        />
-                      </svg>
-                    </span>
-                    <span class="ml-3">Bookmarks</span>
-                  </router-link>
-                </li>
-              </ul>
-            </div>
-          </sidebar>
-        </div>
-        <div class="flex justify-center w-full h-full p-5">
-          <router-view />
-        </div>
+    <div class="flex-1 flex flex-row bg-gray-100 text-gray-800 h-full">
+      <div class="flex fixed md:relative min-h-full">
+        <sidebar v-if="isLoggedIn()" v-model="showSidebar" bg-color="primary" text-color="white">
+          <div router="true" class="sidebar-content px-4 py-4">
+            <ul class="flext flex-col w-full">
+              <li class="my-px">
+                <router-link
+                  to="/home"
+                  class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-primary-100 hover:text-primary"
+                >
+                  <span class="flex items-center justify-center text-lg">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                      />
+                    </svg>
+                  </span>
+                  <span class="ml-3">Home</span>
+                </router-link>
+              </li>
+              <li class="my-px">
+                <router-link
+                  to="/bookmarks"
+                  class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-primary-100 hover:text-primary"
+                >
+                  <span class="flex items-center justify-center text-lg">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                      />
+                    </svg>
+                  </span>
+                  <span class="ml-3">Bookmarks</span>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </sidebar>
+      </div>
+      <div class="flex justify-center w-full h-full p-5">
+        <router-view />
       </div>
     </div>
   </div>
