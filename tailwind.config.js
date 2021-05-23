039484811aa86fe95a389e3d505f077a3d0a4b7e
Christopher Leggett <chris@leggett.dev>
2021-05-23 10:35:56 -0400

    Whitelists one more set of color css classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './public/**/*.html'],
     options: {
       safelist: {
-        standard: [/primary/, /secondary/, /info/, /danger/, /gray/]
+        standard: [/primary/, /secondary/, /info/, /danger/, /gray/, /warning/]
       }
     }
   },
